63df32951d4627cfd39aefcf86decd7f402c14aa
Tom Leonardsson <tom.leo@hotmail.com>
2015-09-24 19:19:19 +0200

    Simple projectile

--- kontroll/kontroll/kontroll/Projectile.cs
+++ kontroll/kontroll/kontroll/Projectile.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace kontroll
 {
-    class Projectile
+    abstract class Projectile : GameObject
     {
+        public int Damage { private get; set; }
+
+        public Projectile(Vector2 position, float angle, float speed)
+        {
+            this.Position = position;
+            this.Angle = angle;
+            this.Speed = speed;
+
+            Texture = AssetManager.spritesheet;
+
+            Scale = 1f;
+            Color = Color.White;
+        }
+
+        public void CheckCollision()
+        {
+
+        }
+
+        public override void Update()
+        {
+            Position += Velocity;
+
+            CheckCollision();
+
+            base.Update();
+        }
     }
 }
---
+++ kontroll/kontroll/kontroll/SimpleProjectile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace kontroll
+{
+    class SimpleProjectile : Projectile
+    {
+        public SimpleProjectile(Vector2 position, float angle, float speed, Color color)
+            : base(position, angle, speed)
+        {
+            SpriteCoords = new Point(Frame(1, 32), 1);
+            SpriteSize = new Point(8, 8);
+            this.Color = color;
+        }
+    }
+}
